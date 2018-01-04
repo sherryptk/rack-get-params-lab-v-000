503ff217a6db970b20b33e71deab5b1ecbf129b0
Sherry El Garhy <sherryptk@gmail.com>
2018-01-04 23:40:33 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,7 +15,7 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/add/)
-      search_term = req.params["q"]
+      search_term = req.params["item"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
       @@cart.each do |item|

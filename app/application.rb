c23c158a53babc3221dd9b16ddbf310ef5209f4c
Sherry El Garhy <sherryptk@gmail.com>
2018-01-04 23:39:28 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -15,10 +15,10 @@ class Application
       search_term = req.params["q"]
       resp.write handle_search(search_term)
     elsif req.path.match(/cart/)
-      @@items.each do |item|
+      @@cart.each do |item|
         resp.write "#{item}\n"
       end
-      
+
     else
       resp.write "Path Not Found"
     end

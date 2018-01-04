9e62353ca65c3c576396887841c8681f1c106e2c
Sherry El Garhy <sherryptk@gmail.com>
2018-01-04 23:46:23 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -28,7 +28,7 @@ class Application
 
     elsif req.path.match(/cart/)
 
-      if @@cart.is_empty?
+      if @@cart.empty?
         resp.write "Your cart is empty"
       else
       @@cart.each do |item|
